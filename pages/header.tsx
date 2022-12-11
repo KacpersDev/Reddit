a104684867cfdef9347f27249cbc4b487e6c01c7
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-12-11 15:27:05 +0100

    Finished header

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,5 +1,24 @@
+import styles from '../styles/Home.module.css'
+import redditLogo from '../images/logo.png';
+
 export default function Header(){
     return(
-        <div>header</div>
+        <div>
+            <div className={styles.header_container}>
+                <img className={styles.header_logo} src={redditLogo.src} alt={'reddit logo'} />
+                <p className={styles.header_title}>Reddit<span id={styles.minimal}>Minimal</span></p>
+            </div>
+            <div className={styles.header_search}>
+                <input className={styles.search_bar} type="text" placeholder='Search Reddit' />
+            </div>
+            <div className={styles.header_button_container}>
+                <div className={styles.header_button_1}>
+                    <button className={styles.header_button} id={styles.log_in}>Log In</button>    
+                </div>    
+                <div className={styles.header_button_2}>
+                    <button className={styles.header_button} id={styles.app}>Get App</button>    
+                </div>    
+            </div>       
+        </div>
     );    
 }
